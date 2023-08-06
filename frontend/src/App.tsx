09f8d553c8f023589a224612148f92eab70e66e8
Carl Tablante <carlwilliam.tablante@gmail.com>
2023-08-06 14:17:54 +1000

    Added Tailwind and UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,79 @@
-import { useEffect, useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+import { useState } from "react"
 import "./App.css"
-import { infoTracker } from "./parser"
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [page, setPage] = useState("")
+  const [service, setService] = useState("")
+  const [searchString, setSearchString] = useState("")
+
+  const options = [
+    { value: "", text: "Choose search" },
+    { value: "google", text: "Google" },
+    { value: "bing", text: "Bing" },
+  ]
+
+  const formSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+  }
+
+  const handleSearchInput = (event: React.FormEvent<HTMLInputElement>) => {
+    setSearchString(event.currentTarget.value)
+  }
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setService(event.currentTarget.value)
+  }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => {
-    const pageRequest = async () => {
-      const result = await fetch("http://localhost:3000/v1/search")
-      const resultText = await result.text()
+  // useEffect(() => {
+  //   const pageRequest = async () => {
+  //     const result = await fetch("http://localhost:3000/v1/search")
+  //     const resultText = await result.text()
 
-      setPage(resultText)
-    }
+  //     setPage(resultText)
+  //   }
 
-    if (!page) {
-      pageRequest()
-    }
-  })
+  //   if (!page) {
+  //     pageRequest()
+  //   }
+  // })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mx-auth h-screen mt-10">
+        <div className="flex items-center justify-center ">
+          <form
+            className="flex border-2 border-gray-200 rounded"
+            onSubmit={formSubmit}
+          >
+            <select
+              id="service"
+              value={service}
+              onChange={handleSelect}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              {options.map((o) => (
+                <option key={o.value} value={o.value}>
+                  {o.text}
+                </option>
+              ))}
+            </select>
+            <input
+              value={searchString}
+              onChange={handleSearchInput}
+              type="text"
+              className="px-4 py-2 w-80"
+              placeholder="Search..."
+            />
+            <button className="px-4 text-white bg-gray-600 border-l">
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div>
+          <h1>Results</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>{page && infoTracker(page)}</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
